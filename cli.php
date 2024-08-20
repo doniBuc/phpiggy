0986532e685cb2aef0b8efcc55a739dca50c3252
doniBuc <108964551+doniBuc@users.noreply.github.com>
2024-08-20 12:57:43 +0800

    created script for database

---
+++ cli.php
@@ -0,0 +1,61 @@
+<?php
+// Refactoring this code seperate the logic
+// $driver = 'mysql';
+// // method for convert array into string url
+// $config = http_build_query(data: [
+//     "host" => "localhost",
+//     "port" => 3306,
+//     "dbname" => "phpiggy"
+// ], arg_separator: ';');
+
+// $dsn = "{$driver}:{$config}";
+// $username = 'root';
+// $password = '';
+// try {
+//     $db = new PDO($dsn, $username, $password); //create a connection to our db
+// } catch (PDOException $e) // this class of catching error in PDO
+// {
+//     die("Unable to connect to database"); // calling this fn die() cause our script to stop running
+// }
+
+// echo "Connected to database";
+
+include __DIR__ . "/src/Framework/Database.php";
+
+use Framework\Database;
+
+$db = new Database(
+    'mysql',
+    ["host" => "localhost", "port" => 3306, "dbname" => "phpiggy"],
+    'root',
+    ''
+);
+
+try {
+
+    $db->connection->beginTransaction();
+
+    $db->connection->query("INSERT INTO products(name) VALUES ('item7')");
+
+    $search = "item1";
+
+    $query = "SELECT * FROM products WHERE name =:name";
+    // $stmt = $db->connection->query($query, PDO::FETCH_ASSOC);
+    $stmt = $db->connection->prepare($query); // to prevent sql injection but not execute the query, unlike query that immediately executed ()
+
+    $stmt->bindValue('name', 'item7', PDO::PARAM_STR);
+
+    $stmt->execute(); // 1. ->execute([$search]) for placeholder (?) OR ->execute(['name'=>$search]) for name paramater (:name) OR using bindValue()
+
+    var_dump($stmt->fetchAll(PDO::FETCH_OBJ));
+
+    $db->connection->commit();
+} catch (Exception $error) {
+
+    if ($db->connection->inTransaction()) // need to check if does have active transanction
+    {
+        $db->connection->rollBack(); // if transaction is not active this method produce error,
+    }
+
+    echo "Transaction failed";
+}
